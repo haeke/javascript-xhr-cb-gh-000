4e6035f70e2ed99d7725412210158c8ba8af6f97
edwin rivera <waehake@gmail.com>
2018-01-25 16:06:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText);
-  console.log(repos);
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + '"onclick="getCommits(this)">Get Commits</a></li>').join('')}
   </ul`;
   document.getElementById("repositories").innerHTML = repoList;
