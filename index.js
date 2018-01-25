bedc0e1036f19bb51857bcee3ea4371ae2981eb3
edwin rivera <waehake@gmail.com>
2018-01-25 16:10:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ function showCommits() {
   const commits = JSON.parse(this.responseText);
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}
   </ul>`;
+  
+  document.getElementById('commits').innerHTML = commitsList;
 }
 
 function getCommits(el) {
