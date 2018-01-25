44967a156ea8ce2c4112ca36de90ce7a609def86
edwin rivera <waehake@gmail.com>
2018-01-25 15:49:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
 function showRepositories(event, data) {
-  console.log(this.responseText);
+  //this is set to the XMLHttpRequest object that fired the event
+  console.log(this.responseText)
 }
-
+ 
 function getRepositories() {
-  const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories)
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
-  req.send();
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
 }
