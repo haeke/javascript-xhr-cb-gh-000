a2d5153b832bb7deb67d412a1b27a156d242c98e
edwin rivera <waehake@gmail.com>
2018-01-25 15:59:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText);
   console.log(repos);
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}
+  </ul`;
   document.getElementById("repositories").innerHTML = repoList;
 }
 
