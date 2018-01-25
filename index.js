61b24e237c556dcd5fc1ad1a174bb49d5af27dba
edwin rivera <waehake@gmail.com>
2018-01-25 16:17:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,12 +12,11 @@ function showCommits() {
 }
 
 function getCommits(el) {
-  console.log(this);
-  const name = el.dataset.repo;
-  const req = new XMLHttpRequest();
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
   req.addEventListener("load", showCommits)
-  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits');
-  req.send();
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
 }
 
 function getRepositories() {
