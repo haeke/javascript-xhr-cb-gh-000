95a9573e7c4b0a3473c15702c6e69a34716143c7
edwin rivera <waehake@gmail.com>
2018-01-25 15:45:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function getRepositories() {
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories('GET', 'https://api.github.com/octocat/repos'));
+  req.addEventListener('load', showRepositories('GET', 'https://api.github.com/users/octocat/repos'));
   req.send();
 }
 
